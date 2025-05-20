6eb0b47d17f5591f59c08cb8ca63610b8fa47e55
Harshit Kumar <harshitkumar2k03@gmail.com>
2025-05-20 22:51:05 +0530

    `Added basic Java documentation and code example`

---
+++ basic.java
@@ -0,0 +1,41 @@
+// ===================
+// java
+// ===================
+// java is an purely objectt oriented programming language developed by sum microsystems of USA in 1991
+// java is a high level programming language
+// It was originally called jack by james(one of the inventors of java) gaslin
+
+// -----------------------------------------------------------------------------------------------------
+// How JAVA works:
+// java is compiled into to the bytecode and then interpreted to the machine code
+// 1. Java source code is written in .java file
+// 2. Java compiler compiles the .java file and generates .class file
+// 3. Java interpreter reads the .class file and executes the bytecode
+// 4. Java Virtual Machine (JVM) is used to run the bytecode
+// 5. JVM is platform independent
+// 6. JVM is a part of Java Runtime Environment (JRE)
+// 7. JRE is a part of Java Development Kit (JDK)
+// 8. JDK is used to develop Java applications
+// ------------------------------------------------------------------------------------------------------
+// JAVA Installation:
+// JDK = "Java Development Kit" collection of tools used for developing and running java programs
+// 1. Download the JDK from the official website: https://www.oracle.com/java/technologies/javase-jdk11-downloads.html
+// 2. Install the JDK by following the instructions on the website
+
+// JRE = "Java Runtime Environment" is a part of JDK that is used to run Java applications/Programs
+// 1. Download the JRE from the official website: https://www.oracle.com/java/technologies/javase-jre8-downloads.html
+// 2. Install the JRE by following the instructions on the website
+
+// Basic structure of a Java program:
+// 1. Class Declaration
+// 2. Main Method Declaration
+// 3. Print Statement
+// 4. End of Class Declaration
+// 5. End of Main Method Declaration
+// 6. End of Program
+
+public class Basic {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
