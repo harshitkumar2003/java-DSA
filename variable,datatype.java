9a26e470fcdd22d2d17ba1688fb51f391c2e3c9b
Harshit Kumar <harshitkumar2k03@gmail.com>
2025-05-21 23:39:17 +0530

    Added initial content to variable,datatype.java file.

---
+++ variable,datatype.java
@@ -0,0 +1,5 @@
+// ================================
+// // Variablee 
+// A Variable is a named storage location in memory that can hold a value.This value can be changed during the execution of the program. In Java, variables must be declared with a specific data type, which determines the kind of values they can store. The data type also defines the operations that can be performed on the variable.
+// ================================
+
