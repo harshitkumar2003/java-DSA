853f1699d4895d646d3c46979c7a51245c4b6b8e
Harshit Kumar <harshitkumar2k03@gmail.com>
2025-05-21 23:48:51 +0530

    "Updated variable,datatype.java with new comments and content."

--- variable,datatype.java
+++ variable,datatype.java
@@ -1,10 +1,11 @@
 // ================================
-// // Variablee 
+// Variablee 
 // A Variable is a named storage location in memory that can hold a value.This value can be changed during the execution of the program. In Java, variables must be declared with a specific data type, which determines the kind of values they can store. The data type also defines the operations that can be performed on the variable.
 // syntax:
 // data_type variable_name = value;
-
+// ----------------------------------------------------------------------------------------------
 // Rules for declaring a variable name 
+// ----------------------------------------------------------------------------------------------
 // 1. Variable name must start with a letter (a-z, A-Z), underscore (_), or dollar sign ($).
 // 2. Variable name can contain letters, digits (0-9), underscores (_), and dollar signs ($).
 // 3. Variable name cannot start with a digit.
@@ -14,6 +15,11 @@
 // 7. Variable name should be case-sensitive (e.g., myVariable and MyVariable are different).
 // 8. Variable name should not contain special characters (e.g., @, #, %, etc.).
 // 9. Variable name should not be too long or too short.
-// ================================
+// ----------------------------------------------------------------------------------------------
+
+
+// ----------------------------------------------------------------------------------------------
+// Data types 
+
 
 
