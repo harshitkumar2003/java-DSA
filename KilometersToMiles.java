eb084f2e762031794cc0694e4fd3ba9a2d3b0a04
Harshit Kumar <harshitkumar2k03@gmail.com>
2025-05-29 00:36:40 +0530

    `Removed commented out code for KilometersToMiles.java`

--- KilometersToMiles.java
+++ KilometersToMiles.java
@@ -266,22 +266,22 @@ public class Basic {
 
 // -------------------------------------------------------------------------------------------------------
 // 4 Write a Java program to convert Kilometers to miles.
-import java.util.Scanner;
-public class KilometersToMiles {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in); // Create a Scanner object
-           // Prompt the user for input
-        System.out.print("Enter distance in kilometers: ");
-        double kilometers = scanner.nextDouble();
-        // Convert kilometers to miles
-        // 1 kilometer is approximately equal to 0.621371 miles
-        double miles = kilometers * 0.621371;
-        // Print the greeting message
-        System.out.println(kilometers + " kilometers is equal to " + miles + " miles.");
+// import java.util.Scanner;
+// public class KilometersToMiles {
+//     public static void main(String[] args) {
+//         Scanner scanner = new Scanner(System.in); // Create a Scanner object
+//            // Prompt the user for input
+//         System.out.print("Enter distance in kilometers: ");
+//         double kilometers = scanner.nextDouble();
+//         // Convert kilometers to miles
+//         // 1 kilometer is approximately equal to 0.621371 miles
+//         double miles = kilometers * 0.621371;
+//         // Print the greeting message
+//         System.out.println(kilometers + " kilometers is equal to " + miles + " miles.");
 
-        scanner.close(); // Close the Scanner object
-    }
-}
+//         scanner.close(); // Close the Scanner object
+//     }
+// }
 // -------------------------------------------------------------------------------------------------------
 // -------------------------------------------------------------------------------------------------------
 // 5 Write a java program to detect whether a nunber entered by the user is integer or not.
