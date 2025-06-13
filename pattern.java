c35b5d00fb39ad9811a66217e6eb2c84c1686779
Harshit Kumar <harshitkumar2k03@gmail.com>
2025-06-13 12:49:38 +0530

    `Reverted import statement, added comments, and modified loop condition in pattern.java`

--- pattern.java
+++ pattern.java
@@ -1,4 +1,30 @@
-import java.util.Scanner;
+// import java.util.Scanner;
+// This program prints a pattern of asterisks based on user input.
+// *
+// **
+// ***
+// ****
+// *****
+
+//  public class pattern {
+//      public static void main(String[] args)  {     
+//      Scanner sc = new Scanner(System.in); 
+//      System.out.print("Enter the number to be print for pattern: ");
+       
+//     int n = sc.nextInt(); // Read the input number
+
+//     for (int i = 1; i <= n; i++) { // Loop through rows
+//         for (int j = 1; j <= i; j++) { // Loop through columns
+//             System.out.print("*"); // Print the current row number
+//         }
+//         System.out.println(); // Move to the next line after each row
+//     }
+       
+//     }
+    
+//  }
+
+ import java.util.Scanner;
  public class pattern {
      public static void main(String[] args)  {     
      Scanner sc = new Scanner(System.in); 
@@ -7,8 +33,8 @@ import java.util.Scanner;
     int n = sc.nextInt(); // Read the input number
 
     for (int i = 1; i <= n; i++) { // Loop through rows
-        for (int j = 1; j <= i; j++) { // Loop through columns
-            System.out.print("*"); // Print the current row number
+        for (int j = 1; j <= 5; j++) { // Loop through columns
+            System.out.print("*"+" "); // Print the current row number
         }
         System.out.println(); // Move to the next line after each row
     }
