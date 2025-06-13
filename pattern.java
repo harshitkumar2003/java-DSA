4cdee08097fbe0a32c1ffefad85fe08d87cbe720
Harshit Kumar <harshitkumar2k03@gmail.com>
2025-06-13 12:53:49 +0530

    `Changed print statement in pattern.java to print "*" followed by a space.`

--- pattern.java
+++ pattern.java
@@ -15,7 +15,7 @@
 
 //     for (int i = 1; i <= n; i++) { // Loop through rows
 //         for (int j = 1; j <= i; j++) { // Loop through columns
-//             System.out.print("*"); // Print the current row number
+//             System.out.print("*"," "); // Print the current row number
 //         }
 //         System.out.println(); // Move to the next line after each row
 //     }
