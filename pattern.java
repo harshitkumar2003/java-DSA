9458ac84cd534d5056d9e80e11d5da6658471d88
Harshit Kumar <harshitkumar2k03@gmail.com>
2025-06-13 12:31:52 +0530

    creating new file for pattern

---
+++ pattern.java
@@ -0,0 +1,18 @@
+// public class KilometersToMiles {
+//     public static void main(String[] args) {
+//         Scanner scanner = new Scanner(System.in); // Create a Scanner object
+//            // Prompt the user for input
+//         System.out.print("Enter distance in kilometers: ");
+//         double kilometers = scanner.nextDouble();
+//         // Convert kilometers to miles
+//         // 1 kilometer is approximately equal to 0.621371 miles
+//         double miles = kilometers * 0.621371;
+//         double miles = kilometers / 1.6; // Alternative conversion factor
+//         // Print the greeting message
+//         System.out.println(kilometers + " kilometers is equal to " + miles + " miles.");
+
+//         scanner.close(); // Close the Scanner object
+//     }
+// } {
+    
+}
