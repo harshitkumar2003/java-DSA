fbf698900c1072c010b2a57d562b7d13122701d3
Harshit Kumar <harshitkumar2k03@gmail.com>
2025-06-13 13:08:36 +0530

    `Refactored pattern printing code to accept user input for both row count and column count, and modified the pattern to print asterisks on the edges and spaces elsewhere.`

--- pattern.java
+++ pattern.java
@@ -23,27 +23,57 @@
 //     }
     
 //  }
+// ------------------------------------------------------------------------------------------------
 
+
+// ------------------------------------------------------------------------------------------------
+// import java.util.Scanner;
+// * * * * *
+// * * * * *
+// * * * * *
+// * * * * *
+// * * * * *
+//  public class pattern {
+//      public static void main(String[] args)  {     
+//      Scanner sc = new Scanner(System.in); 
+//      System.out.print("Enter the number to be print for pattern: ");
+       
+//     int n = sc.nextInt(); // Read the input number
+
+//     for (int i = 1; i <= n; i++) { // Loop through rows
+//         for (int j = 1; j <= 5; j++) { // Loop through columns
+//             System.out.print("*"+" "); // Print the current row number
+//         }
+//         System.out.println(); // Move to the next line after each row
+//     }
+       
+//     }
+    
+//  }
+// ------------------------------------------------------------------------------------------------
+
+
+// ------------------------------------------------------------------------------------------------
 import java.util.Scanner;
-// * * * * *
-// * * * * *
-// * * * * *
-// * * * * *
-// * * * * *
  public class pattern {
      public static void main(String[] args)  {     
      Scanner sc = new Scanner(System.in); 
      System.out.print("Enter the number to be print for pattern: ");
+
        
     int n = sc.nextInt(); // Read the input number
+    int m = sc.nextInt(5); // Read the input number
 
     for (int i = 1; i <= n; i++) { // Loop through rows
-        for (int j = 1; j <= 5; j++) { // Loop through columns
+        for (int j = 1; j <= m; j++) { // Loop through columns
+            if (i==1 || j == 1 || j == m || i == n) { // Check if it's the first or last column or last row
             System.out.print("*"+" "); // Print the current row number
         }
+        else {
+            System.out.print(" "); // Print space for other positions
+        }
+    }
         System.out.println(); // Move to the next line after each row
     }
-       
-    }
-    
+    }  
  }
