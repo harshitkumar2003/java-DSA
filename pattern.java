c31a80f0b0c92e37378911e2451a13bfa723bd7d
Harshit Kumar <harshitkumar2k03@gmail.com>
2025-06-13 12:53:18 +0530

    `Added commented out pattern and import statement, no functional changes.`

--- pattern.java
+++ pattern.java
@@ -24,7 +24,12 @@
     
 //  }
 
- import java.util.Scanner;
+import java.util.Scanner;
+// * * * * *
+// * * * * *
+// * * * * *
+// * * * * *
+// * * * * *
  public class pattern {
      public static void main(String[] args)  {     
      Scanner sc = new Scanner(System.in); 
