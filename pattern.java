3339991eff0abf82c9041d8608fb8ce8ad5e5fa5
Harshit Kumar <harshitkumar2k03@gmail.com>
2025-06-23 01:35:42 +0530

    `Reverted import statement, added and then removed a commented out pattern printing code, and restored original import statement.`

--- pattern.java
+++ pattern.java
@@ -87,8 +87,30 @@
 
 
 // ------------------------------------------------------------------------------------------------
- import java.util.Scanner;
+// import java.util.Scanner;
+// *
+// **
+// ***
+// ****
+// *****
+//   public class pattern {
+//       public static void main(String[] args)  {     
+//       Scanner sc = new Scanner(System.in); 
+//       System.out.println("Enter the number to be print for pattern: ");
+//       int m = 5; // Read the input number
+//       for (int i = 1; i <= m; i++) { // Loop through rows
+//          for (int j = 1; j <= i; j++) { // Loop through columns
+//              System.out.print("*"); // Print the current row number
+//          }
+//          System.out.println(); // Move to the next line after each row
+//      }
+//     }
+// }
+// ------------------------------------------------------------------------------------------------
 
+
+// ------------------------------------------------------------------------------------------------
+ import java.util.Scanner;
   public class pattern {
       public static void main(String[] args)  {     
       Scanner sc = new Scanner(System.in); 
@@ -102,5 +124,6 @@
      }
     }
 }
+
  
   
