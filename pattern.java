22472d296a2648ab4a7bb8e8687b87dc07444d7d
Harshit Kumar <harshitkumar2k03@gmail.com>
2025-06-23 01:30:26 +0530

    `Added import statement and code to print a pattern of asterisks based on user input.`

--- pattern.java
+++ pattern.java
@@ -87,4 +87,20 @@
 
 
 // ------------------------------------------------------------------------------------------------
+ import java.util.Scanner;
 
+  public class pattern {
+      public static void main(String[] args)  {     
+      Scanner sc = new Scanner(System.in); 
+      System.out.print("Enter the number to be print for pattern: ");
+      int m = 5; // Read the input number
+      for (int i = 1; i <= m; i++) { // Loop through rows
+         for (int j = 1; j <= m; j++) { // Loop through columns
+             System.out.print("*"); // Print the current row number
+         }
+         System.out.println(); // Move to the next line after each row
+     }
+    }
+}
+ 
+  
