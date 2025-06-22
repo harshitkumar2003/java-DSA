6a07865ed62104eea18072b96beb76af23d180ae
Harshit Kumar <harshitkumar2k03@gmail.com>
2025-06-23 01:22:23 +0530

    `Added import statement and restored commented code in pattern.java`

--- pattern.java
+++ pattern.java
@@ -54,28 +54,32 @@
 
 
 // ------------------------------------------------------------------------------------------------
-// import java.util.Scanner;
-//  public class pattern {
-//      public static void main(String[] args)  {     
-//      Scanner sc = new Scanner(System.in); 
-//      System.out.print("Enter the number to be print for pattern: ");
+import java.util.Scanner;
+// *****
+// *   *
+// *   *
+// *   *
+// *   *
+// *****
+ public class pattern {
+     public static void main(String[] args)  {     
+     Scanner sc = new Scanner(System.in); 
+     System.out.print("Enter the number to be print for pattern: ");
 
        
-//     int n = sc.nextInt(); // Read the input number
-//     int m = 5; // Read the input number
+    int n = sc.nextInt(); // Read the input number
+    int m = 5; // Read the input number
 
-//     for (int i = 1; i <= n; i++) { // Loop through rows
-//         for (int j = 1; j <= m; j++) { // Loop through columns
-//             if (i == 1 || j == 1 || j == m || i == n) { // Check if it's the first or last column or last row
-//             System.out.print("*"); // Print the current row number
-//         }
-//         else {
-//             System.out.print(" "); // Print space for other positions
-//         }
-//     }
-//         System.out.println(); // Move to the next line after each row
-//     }
-//     }  
-//  }
-// ------------------------------------------------------------------------------------------------
-
+    for (int i = 1; i <= n; i++) { // Loop through rows
+        for (int j = 1; j <= m; j++) { // Loop through columns
+            if (i == 1 || j == 1 || j == m || i == n) { // Check if it's the first or last column or last row
+            System.out.print("*"); // Print the current row number
+        }
+        else {
+            System.out.print(" "); // Print space for other positions
+        }
+    }
+        System.out.println(); // Move to the next line after each row
+    }
+    }  
+ }
