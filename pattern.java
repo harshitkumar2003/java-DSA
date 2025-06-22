b01aba5a5d8b4975d3768856c7e2fa82cbc1e26d
Harshit Kumar <harshitkumar2k03@gmail.com>
2025-06-23 01:33:39 +0530

    `Modified pattern.java: changed input prompt, loop condition for columns`

--- pattern.java
+++ pattern.java
@@ -92,10 +92,10 @@
   public class pattern {
       public static void main(String[] args)  {     
       Scanner sc = new Scanner(System.in); 
-      System.out.print("Enter the number to be print for pattern: ");
+      System.out.println("Enter the number to be print for pattern: ");
       int m = 5; // Read the input number
       for (int i = 1; i <= m; i++) { // Loop through rows
-         for (int j = 1; j <= m; j++) { // Loop through columns
+         for (int j = 1; j <= i; j++) { // Loop through columns
              System.out.print("*"); // Print the current row number
          }
          System.out.println(); // Move to the next line after each row
