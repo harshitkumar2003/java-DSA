4c5d86a2edb8dc4857913bff65a18a9b9a047837
Harshit Kumar <harshitkumar2k03@gmail.com>
2025-06-24 01:03:32 +0530

    `Added new pattern printing functionality with even/odd row check`

--- pattern.java
+++ pattern.java
@@ -226,6 +226,55 @@
 //         }
 // ------------------------------------------------------------------------------------------------
 
+// ------------------------------------------------------------------------------------------------
+//  import java.util.Scanner;
+// 1 
+// 2 3
+// 4 5 6
+// 7 8 9 10
+// 11 12 13 14 15
+
+//   public class pattern {
+//       public static void main(String[] args) {     
+//       Scanner sc = new Scanner(System.in); 
+//       System.out.println("Enter the number to be print for pattern: ");
+//          int m = sc.nextInt(); // Read the input number
+//          int num = 1;
+//         for (int i = 1; i <= m; i++) { // Loop through rows
+//             for (int j = 1; j <= i; j++) { // Loop through columns for spaces         
+//                 System.out.print(num+" "); // Print the current row number
+//                 num++;
+//             } 
+//             System.out.println(); // Move to the next line after each row
+//             }
+//           } 
+//         }
+// ------------------------------------------------------------------------------------------------
+
+
+// ------------------------------------------------------------------------------------------------
+ import java.util.Scanner;
+
+  public class pattern {
+      public static void main(String[] args) {     
+      Scanner sc = new Scanner(System.in); 
+      System.out.println("Enter the number to be print for pattern: ");
+         int m = 5; // Read the input number
+        for (int i = 1; i <= m; i++) { // Loop through rows
+            for (int j = 1; j <= i; j++) { // Loop through columns for spaces  
+                if(i % 2 != 0) { // Check if the row number is even
+                    System.out.print("1"); // Print space for even rows
+                } else {
+                System.out.print("0"); // Print the current row number
+            } 
+            System.out.println(); // Move to the next line after each row
+            }
+          } 
+        }
+    }
+// ------------------------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------------------------
+
 
  
   
