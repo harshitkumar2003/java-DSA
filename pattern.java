94f8745d1bc560aad158aac8cce5eef639c2b041
Harshit Kumar <harshitkumar2k03@gmail.com>
2025-06-24 00:38:55 +0530

    `Added commented out code for printing a specific pattern`

--- pattern.java
+++ pattern.java
@@ -159,8 +159,25 @@
 
 
 // ------------------------------------------------------------------------------------------------
-
-
+//  import java.util.Scanner;
+// 1
+// 22
+// 333
+// 4444
+// 55555
+//   public class pattern {
+//       public static void main(String[] args) {     
+//       Scanner sc = new Scanner(System.in); 
+//       System.out.println("Enter the number to be print for pattern: ");
+//         int m = 5; // Read the input number
+//         for (int i = 1; i <= m; i++) { // Loop through rows
+//             for (int j = 1; j <= i; j++) { // Loop through columns for spaces         
+//                 System.out.print(i); // Print the current row number
+//             } 
+//             System.out.println(); // Move to the next line after each row
+//             }
+//           } 
+//         }
 // ------------------------------------------------------------------------------------------------
 
 
