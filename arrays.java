7f0a6e2b93a7ca62dae9e85622083994eeb0558a
Harshit Kumar <harshitkumar2k03@gmail.com>
2025-07-29 23:18:32 +0530

    "Removed main method and print statements from arrays.java"

--- arrays.java
+++ arrays.java
@@ -1,14 +1,2 @@
 import java.util.*;
-public class arrays {
-    public static void main(String[] args) {
-        int[] arr = {3, 1, 4, 5, 2};
-        for (int i = 0; i < 5; i++) {
-            if(arr[i] >= arr[i + 1]){
-                int temp = arr[i];
-                arr[i] = arr[i + 1];
-                arr[i + 1] = temp;
-            }
-            System.out.println(arr[i]);
-        }
-    }    
-}
+
