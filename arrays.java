0e3dea1ed6b5e6b631a6ab25965b37763f6c90d8
Harshit Kumar <harshitkumar2k03@gmail.com>
2025-07-29 23:05:19 +0530

    `Refactored main method in arrays.java: changed array values, added swap logic, and modified loop condition.`

--- arrays.java
+++ arrays.java
@@ -1,7 +1,13 @@
+import java.util.*;
 public class arrays {
     public static void main(String[] args) {
-        int[] arr = {1, 2, 3, 4, 5};
-        for (int i = 0; i < arr.length; i++) {
+        int[] arr = {3, 1, 4, 5, 2};
+        for (int i = 0; i < 5; i++) {
+            if(arr[i] >= arr[i + 1]){
+                int temp = arr[i];
+                arr[i] = arr[i + 1];
+                arr[i + 1] = temp;
+            }
             System.out.println(arr[i]);
         }
     }    
