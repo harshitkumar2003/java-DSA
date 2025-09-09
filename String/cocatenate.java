a5d4be6d1cf8765a266f219b44013e861a885b80
Harshit Kumar <harshitkumar2k03@gmail.com>
2025-09-09 22:07:49 +0530

    `Added concatenation functionality and string length calculation to cocatenate.java`

--- String/cocatenate.java
+++ String/cocatenate.java
@@ -1,11 +1,18 @@
 package String;
-
 public class cocatenate {
     public static void main(String[] args) {
+        // concatenation
         String first = "Harshit";
-        String last = " Kumar";
+        String last = "Kumar";
+        String fullname = first + " " + last;
+        System.out.println(fullname); // Harshit Kumar
 
-        String fullname = first + last;
-        System.out.println(fullname);
+        // this function calculate the length of a string
+        System.out.println(fullname.length()); // 13 including white space
+
+        // this function read the full string character by character using for loop
+        for(int i = 0; i < fullname.length(); i++){
+            System.out.println(fullname.charAt(i));
+        }
     }
 }
