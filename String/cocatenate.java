a5eb9740b44cbebc748cc42679d67d66f19e9a39
Harshit Kumar <harshitkumar2k03@gmail.com>
2025-09-09 21:45:14 +0530

    Remove unnecessary blank line in cocatenate.java

--- String/cocatenate.java
+++ String/cocatenate.java
@@ -4,8 +4,8 @@ public class cocatenate {
     public static void main(String[] args) {
         String first = "Harshit";
         String last = " Kumar";
+
         String fullname = first + last;
         System.out.println(fullname);
-        
     }
 }
