fcfa4e5d84696c142fe9b6bb03abe692106f875e
Harshit Kumar <harshitkumar2k03@gmail.com>
2025-09-09 23:10:10 +0530

    `Added variable declaration and substring call with offset, removed commented out code`

--- String/substring.java
+++ String/substring.java
@@ -3,7 +3,11 @@ package String;
 public class substring {
     public static void main(String[] args) {
         // substring
-        String 
+        String sentence = "My name is Harshit";
+    //    String name = sentence.substring(11, sentence.length());
+       String name = sentence.substring(11);
+       System.out.println(name);
+
     }
     
 }
