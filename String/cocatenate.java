56dab486a259a01bbe3ca390566e07035bf9888c
Harshit Kumar <harshitkumar2k03@gmail.com>
2025-09-09 21:42:54 +0530

    Refactor strings.java for improved formatting and add cocatenate.java for string concatenation example

---
+++ String/cocatenate.java
@@ -0,0 +1,11 @@
+package String;
+
+public class cocatenate {
+    public static void main(String[] args) {
+        String first = "Harshit";
+        String last = " Kumar";
+        String fullname = first + last;
+        System.out.println(fullname);
+        
+    }
+}
--- String/strings.java
+++ String/strings.java
@@ -5,8 +5,9 @@ import java.util.Scanner;
 public class strings {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String name = sc.nextLine();
-        System.out.println("Your name is : " + name);
+         String name = sc.nextLine();
+         System.out.println("Your name is : " + name);
+         String sentence = sc.nextLine();
+         System.out.println("Your sentence :" + sentence);
     }
-
 }
