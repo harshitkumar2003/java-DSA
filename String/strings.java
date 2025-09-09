cf4c3205eaf69e0e61df22ce2a500a47a843fb66
Harshit Kumar <harshitkumar2k03@gmail.com>
2025-09-09 20:35:30 +0530

    Add strings.java for user input and output functionality

---
+++ String/strings.java
@@ -0,0 +1,12 @@
+package String;
+
+import java.util.Scanner;
+
+public class strings {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        System.out.println("Your name is : " + name);
+    }
+
+}
