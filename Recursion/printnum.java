a1ecd0f02266cae7458575404267abab0077b802
Harshit Kumar <harshitkumar2k03@gmail.com>
2025-10-05 23:08:08 +0530

    Fix print method signature in printnum.java to return void

--- Recursion/printnum.java
+++ Recursion/printnum.java
@@ -1,13 +1,14 @@
 package Recursion;
 
 public class printnum {
-    public static int print(int n){
+
+    public static void print(int n){
         if(n==0){
-            return 0;
+            return;
         }
         System.out.println(n);
-        return n-1;
-}
+        print(n-1);
+    }
     public static void main(String[] args) {
         int n=5;
         print(n);
