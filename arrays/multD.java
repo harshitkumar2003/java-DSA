22c8774f3a67ee87cae43b3fe4351f37064bb551
Harshit Kumar <harshitkumar2k03@gmail.com>
2026-01-17 01:19:49 +0530

    adding sume questions in one dimansional java file wit some properties

--- arrays/multD.java
+++ arrays/multD.java
@@ -16,5 +16,24 @@ public class multD {
     //  dataType[][] arrayName = new dataType[rows][columns];
     int[][] m1 = new int[2][3];
 
+    //! 3. Initializing an Array
+    // a) By assigning values later
+    int[][] matrix = new int[2][3];
+    matrix[0][0] = 1;
+    matrix[0][1] = 2;
+    matrix[0][2] = 3;
+    matrix[1][0] = 4;
+    matrix[1][1] = 5;
+    matrix[1][2] = 6;
+    System.out.println(matrix[0][1]);
+    
+    // b) At the time of declaration
+    int[][] matrix1 = {
+        {1, 2, 3},
+        {4, 5, 6}
+    };
+    System.out.println(matrix1[0][2]);
+
+
     }
 }
--- arrays/oneD.java
+++ arrays/oneD.java
@@ -26,11 +26,69 @@ public class oneD {
     num[0] = 10;
     num[1] = 20;
     num[2] = 30;
-    System.out.println(num[2]);
 
+    //! For loop for displaying the array using (.length) property
+    for (int i = 0; i < num.length; i++) {
+      // System.out.println(num[i]);
+    }
+    
     // b) At the time of declaration
-    int[] val = {10, 20, 30};
-    System.out.println(val[1]);
+    int[] val = {10, 20, 30, 40, 50};
+
+// -------------------------------------------------------------------------------
+    //! Enhanced for loop -> only works on collection (group of numbers or array)
+// -------------------------------------------------------------------------------
+  // Syntax
+  //!  for (iterable_type iterable_element : iterable) {  }
+  //TODO_ iterable_type -> datatype | iterable_element -> number itslef not a index | iterable -> array name
+  
+  for(int a : val) {
+    System.out.print(a + " ");
+  }
+
+// -------------------------------------------------------------------------------
+  // Ques1. WAP to search an element in an array 
+   int[] arr = {12, 14, 22, 42, 55};
+   
+  //  for (int i = 0 ; i < arr.length ; i++) 
+  //! Enhanced for loop
+  for (int i : arr) {
+       if (i == 12) {
+        System.out.println("\nFound");
+    }    
+   }
+
+// -------------------------------------------------------------------------------
+  // Ques2. WAP to find maximum element in an array
+  int[] fm = new int[5];
+  fm[0] = 11;
+  fm[1] = 55;
+  fm[2] = 66;
+  fm[3] = 33;
+  fm[4] = 32;
+  int res = Integer.MIN_VALUE; //! take the smallest value
+
+  //! Enhanced for loop
+  for (int i : fm) {
+    if (i > res) {
+      res = i;
+    }
+  }
+    System.out.println(res);
+  
+// -------------------------------------------------------------------------------
+  // Ques3. WAP to reverse an array 
+  int[] rev = {1, 3, 4, 6, 8};
+  int sum = 0;
+  for (int i = rev.length-1 ; i >= 0 ; i--) {
+      System.out.print(rev[i] + " ");
+    }
+  //! Enhanced for loop
+  for (int i : rev) {
+      sum += i;
+    }
+    System.out.println("\n"+ sum);
+// -------------------------------------------------------------------------------
 
   }
  
