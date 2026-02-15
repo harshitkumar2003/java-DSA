8f84c3ea16e28c2c65915768454c43f70678904c
Harshit Kumar <harshitkumar2k03@gmail.com>
2026-02-16 00:10:18 +0530

    Hands on with Exception handling

--- core-java/Exceptionhandling/Exc.java
+++ core-java/Exceptionhandling/Exc.java
@@ -13,18 +13,52 @@ try-catch with finally
 ! The finally block always executed whether an exception is thrown or not. The finally is used for closing resources like db connections, open files and network connections, It is used after a try-catch block to execute code that must run.
 
 throw and throws Keywords
-
 ! 1. throw: Used to explicitly throw a single exception. We use throw when something goes wrong (or “shouldn’t happen”) and we want to stop normal flow and hand control to exception handling.
 
 ! 2. throws: Declares exceptions that a method might throw, informing the caller to handle them. It is mainly used with checked exceptions (explained below). If a method calls another method that throws a checked exception, and it doesn’t catch it, it must declare that exception in its throws clause
+
 Hierarchy
-Checked & Unchecked Exceptions
+Throwable
+   ├── Error (serious issues, not meant to be handled)
+   │     ├── OutOfMemoryError
+   │     ├── StackOverflowError
+   │     └── ...
+   └── Exception (conditions applications may handle)
+         ├── Checked Exceptions
+         │       ├── IOException
+         │       ├── SQLException
+         │       ├── ClassNotFoundException
+         │       └── ...
+         └── Unchecked Exceptions (RuntimeException and its subclasses)
+                 ├── NullPointerException
+                 ├── ArithmeticException
+                 ├── ArrayIndexOutOfBoundsException
+                 └── ...
+
+Checked 
+! Checked at compile-time.
+! Must be declared in method signatures using throws or handled with try-catch.
+
+Unchecked Exceptions (Runtime Exceptions)
+! Checked at runtime.
+! Do not need to be declared or caught explicitly.
+
 Custom Exceptions
  */
 public class Exc {
 
     public static void main(String[] args) {
 
+        int[] arr = {2,1,22,34};
+
+        try {
+            System.out.println(arr[4]);
+            
+        } catch (Exception e) {
+            System.out.println("Array size kam kar 0 se suru hota hai..." + e);
+        }
+        System.out.println(arr[1]);
+ 
     }
 
 }
